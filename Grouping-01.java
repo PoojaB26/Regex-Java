d608d2bb2c1710a5216f14239dc7411e5fa09acb
Pooja Bhaumik <pbhaumik26@gmail.com>
2017-01-20 00:02:08 +0530

    Update Grouping-01.java

--- Grouping-01.java
+++ Grouping-01.java
@@ -1,1 +1,9 @@
+public class Solution {    
 
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\b[aeouiAEIOU][a-zA-Z]*\\b"); // Use \\ instead of using \ 
+    
+    }
+}
